944e61c666b4bf1fdb8290d5c71c2185d4f1e2be
Justin Chao <justin.chao@optum.com>
2019-05-25 16:03:26 -0400

    adding env and configmap

--- flask_nginx/flask/app.py
+++ flask_nginx/flask/app.py
@@ -1,9 +1,22 @@
 from flask import Flask
+import os
+import json
+import uwsgi
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World in Production!'
+    if os.environ.get('SECRET') is None:
+        greeting = '<h1>Hello World in Production!</h1> <br/>' \
+                + 'Number of uWSGI workers: ' + str(uwsgi.numproc) + '<br/>'  \
+                + os.getenv('MY_ENV_VAR', "no environment variables found. <br/>")
+    else:
+        greeting = '<h1>Hello World in Production!</h1> <br/>' \
+                + 'Number of uWSGI workers: ' + str(uwsgi.numproc) + '<br/>' \
+                + os.getenv('MY_ENV_VAR', "no environment variables found. <br/>") + \
+                + 'I have a secret: ' + str(os.environ.get('SECRET'))
+
+    return greeting 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
