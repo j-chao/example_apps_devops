d29d995b2941340a514cf434f6d4c191e02621d6
Justin Chao <justin.chao@optum.com>
2020-01-20 17:01:26 -0500

    refactoring to use spring webflux reactive programming model

--- example_microservices/spring_demo/src/main/java/com/example/demo/persistence/PatientRepository.java
+++ example_microservices/spring_demo/src/main/java/com/example/demo/persistence/PatientRepository.java
@@ -1,10 +1,11 @@
 package com.example.demo.persistence;
 
 import com.example.demo.model.Patient;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-  List<Patient> findByLastName(String lastName);
-  Patient findById(long id);
+@Configuration
+public interface PatientRepository extends ReactiveCrudRepository<Patient, Long> {
+
+  //  Mono<Patient> findByLastName(String lastName);
 }
